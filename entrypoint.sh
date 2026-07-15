7e62cb4780a03c913f5d8cd71d23f8f067706566
Alex Obese-Jecty <87595259+laboneAlex@users.noreply.github.com>
2026-07-15 14:55:37 +0200

    Update entrypoint.sh
    Populate with relevant code block

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# If the SNOWFLAKE_PRIVATE_KEY secret is present, write it safely to temporary memory
+if [ -n "$SNOWFLAKE_PRIVATE_KEY" ]; then
+  echo "$SNOWFLAKE_PRIVATE_KEY" > /tmp/snowflake_key.p8
+  chmod 600 /tmp/snowflake_key.p8
+fi
+
+# Hand execution back over to the original Airflow startup scripts
+exec /entrypoint "$@"
